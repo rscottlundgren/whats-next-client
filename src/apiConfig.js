16042aff9788d2ae8452a09a976658ed7fe7aa08
rscottlundgren <rscottlundgren@gmail.com>
2020-05-15 12:36:08 -0400

    Update Config - Updated Config

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://gentle-cliffs-56174.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
