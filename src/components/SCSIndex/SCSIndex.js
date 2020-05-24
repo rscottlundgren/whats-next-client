b7cb94ffdc2ef587ffcc15fc594c90fce6c9d93b
rscottlundgren <rscottlundgren@gmail.com>
2020-05-24 08:48:12 -0400

    Change

--- src/components/SCSIndex/SCSIndex.js
+++ src/components/SCSIndex/SCSIndex.js
@@ -98,7 +98,7 @@ class SCSIndex extends Component {
     return (
       <div>
         <h1>Single Card Spread Reading Listing</h1>
-        <h3>Don't see any spreads? Make One Above!</h3>
+        <h3>(If You Do Not See Any Spreads Below, Make One Above)</h3>
         {spreadIndexJsx}
       </div>
     )
